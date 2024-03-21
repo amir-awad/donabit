1ff33e483b4c9944f0d70aabb8def7c2d23831fc
amir-awad <amirawad004@gmail.com>
2024-03-21 07:46:17 +0200

    feat: added CustomizedController component

---
+++ client/src/components/CustomizedController.tsx
@@ -0,0 +1,39 @@
+import { Controller } from 'react-hook-form';
+import { Grid, Typography } from '@mui/material';
+
+interface CustomizedControllerProps {
+	name: string;
+	label: string;
+	control: any;
+	children: (field: any) => JSX.Element;
+}
+
+const CustomizedController = ({
+	name,
+	label,
+	control,
+	children,
+}: CustomizedControllerProps) => (
+	<Controller
+		name={name}
+		control={control}
+		render={({ field }) => (
+			<>
+				<Grid container spacing={1}>
+					<Grid item>
+						<Typography
+							variant='body2'
+							noWrap
+							sx={{ fontWeight: 'bold', fontSize: 16, mb: 1 }}
+						>
+							{label}
+						</Typography>
+					</Grid>
+				</Grid>
+				{children(field)}
+			</>
+		)}
+	/>
+);
+
+export default CustomizedController;
--- client/src/components/EditForm.tsx
+++ client/src/components/EditForm.tsx
@@ -16,10 +16,9 @@ import {
 	Typography,
 } from '@mui/material';
 
-import {
-	HelpOutlineOutlined as HelpIcon,
-	ClearOutlined as ClearIcon,
-} from '@mui/icons-material';
+import { ClearOutlined as ClearIcon } from '@mui/icons-material';
+
+import CustomizedController from './CustomizedController';
 
 interface EditFormProperties {
 	supporterName: string;
@@ -42,13 +41,23 @@ interface EditFormValues {
 	frequency: string;
 }
 
+const CampaignNameOptions = [
+	'My awesome campaign #6',
+	'Campaign for the future',
+	'Campaign for the past',
+	'Campaign for the present',
+];
+const DesignationOptions = ['General', 'Specific', '___'];
 const donationFrequencyOptions = ['One time', 'Monthly', 'Quarterly', 'Yearly'];
 
 const donationInfoSchema = z.object({
-	supporter: z.string().trim().min(1, 'Supporter name is required'),
-	campaign: z.string().trim().min(1, 'Campaign name is required'),
-	designation: z.string().trim().min(1, 'Designation is required'),
-	frequency: z.string().trim().min(1, 'Frequency is required'),
+	supporterName: z
+		.string()
+		.trim()
+		.min(3, 'Supporter name must be at least 3 characters'),
+	campaignName: z.string().min(1, 'Campaign name is required'),
+	designation: z.string().min(1, 'Designation is required'),
+	frequency: z.string().min(1, 'Frequency is required'),
 });
 
 const EditForm: React.FC<EditFormProperties> = ({
@@ -65,162 +74,152 @@ const EditForm: React.FC<EditFormProperties> = ({
 	open,
 }) => {
 	const {
+		handleSubmit,
+		control,
 		formState: { errors },
 	} = useForm({
 		resolver: zodResolver(donationInfoSchema),
-		defaultValues: { supporterName, campaignName, designation, frequency },
+		defaultValues: {
+			supporterName,
+			campaignName,
+			designation,
+			frequency,
+		},
 	});
 
 	return (
 		<Dialog open={open || false} onClose={onClose}>
-			<DialogTitle>
-				<Grid container spacing={1}>
-					<Grid item>
-						<Typography
-							variant='h6'
-							sx={{ fontWeight: 'bold', ml: 2, fontSize: 20 }}
-						>
-							Donation Information
-						</Typography>
-					</Grid>
-					<Grid item sx={{ ml: 'auto' }}>
-						<Button
-							onClick={onClose}
-							startIcon={
-								<ClearIcon sx={{ color: 'grey', width: 20, height: 20 }} />
-							}
-							sx={{ color: 'black', fontWeight: 'bold' }}
-						/>
-					</Grid>
-				</Grid>
-			</DialogTitle>
-			<DialogContent>
-				<Grid container spacing={2} sx={{ p: 2, maxWidth: 400 }}>
-					<Grid item xs={12}>
-						<Grid container spacing={1}>
-							<Grid item>
-								<Typography
-									variant='body2'
-									noWrap
-									sx={{ fontWeight: 'bold', fontSize: 16, mb: 1 }}
-								>
-									Supporter Name
-								</Typography>
-							</Grid>
-							<Grid item>
-								<HelpIcon fontSize='small' sx={{ color: 'grey' }} />
-							</Grid>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<DialogTitle>
+					<Grid container spacing={1}>
+						<Grid item>
+							<Typography
+								variant='h6'
+								sx={{ fontWeight: 'bold', ml: 2, fontSize: 20 }}
+							>
+								Donation Information
+							</Typography>
 						</Grid>
-
-						<TextField
-							value={supporterName}
-							error={!!errors.supporterName}
-							helperText={errors.supporterName?.message}
-							fullWidth
-							required
-							onChange={(e) => setSupporterName(e.target.value)}
-						/>
+						<Grid item sx={{ ml: 'auto' }}>
+							<Button
+								onClick={onClose}
+								startIcon={
+									<ClearIcon sx={{ color: 'grey', width: 20, height: 20 }} />
+								}
+								sx={{ color: 'black', fontWeight: 'bold' }}
+							/>
+						</Grid>
 					</Grid>
-					<Grid item xs={12}>
-						<FormControl fullWidth>
-							<Typography
-								variant='body2'
-								noWrap
-								sx={{ fontWeight: 'bold', fontSize: 16, mb: 1 }}
+				</DialogTitle>
+				<DialogContent>
+					<Grid container spacing={2} sx={{ p: 2, maxWidth: 400 }}>
+						<Grid item xs={12}>
+							<CustomizedController
+								name='supporterName'
+								label='Supporter Name'
+								control={control}
 							>
-								Campaign
-							</Typography>
-							<Select
-								labelId='campaign-select-label'
-								value={campaignName}
-								error={!!errors.campaignName}
-								required
-								onChange={(e) => setCampaignName(e.target.value)}
+								{(field) => (
+									<TextField
+										value={field.value}
+										fullWidth
+										required
+										error={!!errors.supporterName}
+										helperText={errors.supporterName?.message}
+										onChange={(e) => {
+											field.onChange(e);
+											setSupporterName(e.target.value);
+										}}
+									/>
+								)}
+							</CustomizedController>
+						</Grid>
+						<Grid item xs={12}>
+							<CustomizedController
+								name='campaignName'
+								label='Campaign Name'
+								control={control}
 							>
-								{[
-									'My awesome campaign #1',
-									'My awesome campaign #2',
-									'My awesome campaign #3',
-									'My awesome campaign #4',
-									'My awesome campaign #5',
-									'My awesome campaign #6',
-								].map((option) => (
-									<MenuItem key={option} value={option}>
-										{option}
-									</MenuItem>
-								))}
-							</Select>
-						</FormControl>
+								{(field) => (
+									<FormControl fullWidth>
+										<Select
+											{...field}
+											required
+											onChange={(e) => {
+												field.onChange(e);
+												setCampaignName(e.target.value as string);
+											}}
+										>
+											{CampaignNameOptions.map((option) => (
+												<MenuItem key={option} value={option}>
+													{option}
+												</MenuItem>
+											))}
+										</Select>
+									</FormControl>
+								)}
+							</CustomizedController>
+						</Grid>
+						<Grid item xs={12}>
+							<CustomizedController
+								name='designation'
+								label='Designation'
+								control={control}
+							>
+								{(field) => (
+									<FormControl fullWidth>
+										<Select
+											{...field}
+											required
+											onChange={(e) => {
+												field.onChange(e);
+												setDesignation(e.target.value as string);
+											}}
+										>
+											{DesignationOptions.map((option) => (
+												<MenuItem key={option} value={option}>
+													{option}
+												</MenuItem>
+											))}
+										</Select>
+									</FormControl>
+								)}
+							</CustomizedController>
+						</Grid>
+						<Grid item xs={12}>
+							<CustomizedController
+								name='frequency'
+								label='Frequency'
+								control={control}
+							>
+								{(field) => (
+									<FormControl fullWidth>
+										<Select
+											{...field}
+											required
+											onChange={(e) => {
+												field.onChange(e);
+												setFrequency(e.target.value as string);
+											}}
+										>
+											{donationFrequencyOptions.map((option) => (
+												<MenuItem key={option} value={option}>
+													{option}
+												</MenuItem>
+											))}
+										</Select>
+									</FormControl>
+								)}
+							</CustomizedController>
+						</Grid>
 					</Grid>
-					<Grid item xs={12}>
-						<FormControl fullWidth>
-							<Typography
-								variant='body2'
-								noWrap
-								sx={{ fontWeight: 'bold', fontSize: 16, mb: 1 }}
-							>
-								Designation
-							</Typography>
-
-							<Select
-								labelId='designation-select-label'
-								value={designation}
-								error={!!errors.designation}
-								required
-								onChange={(e) => setDesignation(e.target.value)}
-							>
-								{[
-									'General Fund',
-									'Building Fund',
-									'Education Fund',
-									'Health Fund',
-									'Food Fund',
-									'____',
-								].map((option) => (
-									<MenuItem key={option} value={option}>
-										{option}
-									</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-					</Grid>
-					<Grid item xs={12}>
-						<FormControl fullWidth>
-							<Typography
-								variant='body2'
-								noWrap
-								sx={{ fontWeight: 'bold', fontSize: 16, mb: 1 }}
-							>
-								Frequency
-							</Typography>
-							<Select
-								labelId='frequency-select-label'
-								value={frequency}
-								error={!!errors.frequency}
-								required
-								onChange={(e) => setFrequency(e.target.value)}
-							>
-								{donationFrequencyOptions.map((option) => (
-									<MenuItem key={option} value={option}>
-										{option}
-									</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-					</Grid>
-				</Grid>
-			</DialogContent>
-			<DialogActions>
-				<Button
-					variant='contained'
-					onClick={() =>
-						onSubmit({ supporterName, campaignName, designation, frequency })
-					}
-					sx={{ m: 3, width: 100 }}
-				>
-					Save
-				</Button>
-			</DialogActions>
+				</DialogContent>
+				<DialogActions>
+					<Button variant='contained' type='submit' sx={{ m: 3, width: 100 }}>
+						Save
+					</Button>
+				</DialogActions>
+			</form>
 		</Dialog>
 	);
 };
