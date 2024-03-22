82180b7e6b5cc6aa3b13ca5c933d1004d70736cf
amir-awad <amirawad004@gmail.com>
2024-03-22 06:05:35 +0200

    test: added tests to GET GET /donations/:id/details

--- server/src/tests/api-tests/donationTests.test.ts
+++ server/src/tests/api-tests/donationTests.test.ts
@@ -60,3 +60,26 @@ describe('GET /donations/:id/information', () => {
     expect(information.status).toBe(STATUS_CODES.NOT_FOUND);
   });
 });
+
+describe('GET /donations/:id/details', () => {
+  it('should return 200 when fetching donation details', async () => {
+    const donation = generateDonation();
+    await request(app).post('/donations').send(donation);
+    const response = await request(app).get('/donations');
+    const lastDonation = response.body[response.body.length - 1];
+    expect(lastDonation.supporter_name).toBe(donation.supporterName);
+    expect(lastDonation.campaign).toBe(donation.campaignName);
+    expect(lastDonation.designation).toBe(donation.designation);
+    expect(lastDonation.frequency).toBe(donation.frequency);
+  });
+
+  it('should return 500 when fetching donation details with invalid ID', async () => {
+    const details = await request(app).get('/donations/invalid-id/details');
+    expect(details.status).toBe(STATUS_CODES.INTERNAL_SERVER_ERROR);
+  });
+
+  it('should return 404 when fetching donation details with non-existing ID', async () => {
+    const details = await request(app).get('/donations/00000000-0000-0000-0000-000000000000/details');
+    expect(details.status).toBe(STATUS_CODES.NOT_FOUND);
+  });
+});
